de1159a63b6929aaf33c63af423fbe1c08428226
Ryan Tan <skyfish81@gmail.com>
2017-09-03 23:52:25 +0800

    Tried Functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,11 +12,11 @@ object Main extends App {
 
   println(r.calories)
 
-  val s1 = new Salad(5,5)
-  val s2 = new Salad(15,15)
-  val dinner = new Dinner(List(s1,s2))
+  val s1 = new Salad(5, 5)
+  val s2 = new Salad(15, 15)
+  val dinner = new Dinner(List(s1, s2))
 
-  println(s"Number of Menu Items = ${dinner.numberOfMenuItems}")
+  println(s"Number of Menu Items = ${dinner.numberOfMenuItems()}")
 
   println(WATERMELON_CALORIES)
 
@@ -31,5 +31,22 @@ object Main extends App {
   val j = new JapaneseGreetings with DefaultGreetings
   println(j.sayHello) //outputs konnichiwa
   println(j.defaultHello) //outputs Hello
+
+  println("========= FUNCTIONS ==========")
+  //creates a function that takes an Int as a parameter and returns
+  // Int.The variable type in Scala is formally declared
+  val succ = (foo: Int) => {
+    foo + 1
+  }
+
+  println(succ(10)) //outputs 11
+  val c = succAndLog(10, (i: Int) => i + 1) //increment 10 and returns
+
+  def succAndLog(someInt: Int, succ: Int => Int) = {
+    println(s"Incrementing $someInt")
+    succ(someInt)
+  }
+
+  println(c)
 
 }
